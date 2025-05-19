7ce00549251d6d88355220800a64f1501a7b9805
ataitler <ataitler@gmail.com>
2025-05-19 09:41:24 +0300

    main

--- main.py
+++ main.py
@@ -12,21 +12,6 @@ parser.add_argument("--average", type=int, help="How many episode to average tes
 args = parser.parse_args()
 
 
-# EXPERIMENT_NAME = ['instance1', 'instance3','instance10']
-# RDDL_SUFFIX = '.rddl'
-# EXPERIMENT_INSTANCE = [name + RDDL_SUFFIX for name in EXPERIMENT_NAME]
-# # EXPERIMENT_INSTANCE = ['instance1.rddl', 'instance3.rddl','instance10.rddl']
-# EXPERIMENT_LENGTH = [0,0,1]
-#
-#
-#
-# def main2():
-#     for i in range(len(EXPERIMENT_NAME)):
-#         print('Running experiment ' + EXPERIMENT_NAME[i] + ':')
-#         Manager = ExperimentManager.ExperimentManager(name=EXPERIMENT_NAME[i], instance=EXPERIMENT_INSTANCE[i],
-#                                                       run_from_scratch=False, average=5, train_episodes_interval=1)
-#         Manager.run_experiment(num_train_episodes=EXPERIMENT_LENGTH[i])
-
 
 def main(instance, seed, length, average):
     instance2run = instance+'.rddl'
